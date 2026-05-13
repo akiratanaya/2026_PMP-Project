d63f6b7817d53b149e7957f0518b49ccabe0bd91
akiratanaya <akirakenji78@gmail.com>
2026-05-14 00:13:18 +0700

    feat: add function for uart

---
+++ library/src/arduino_uart.c
@@ -0,0 +1,62 @@
+#include "arduino_uart.h"
+
+#include <avr/io.h>
+
+void uart_init(uint32_t baud_rate)
+{
+    uint16_t ubrr = (uint16_t)((F_CPU / (16UL * baud_rate)) - 1UL);
+
+    UBRR0H = (uint8_t)(ubrr >> 8);
+    UBRR0L = (uint8_t)ubrr;
+    UCSR0A = 0U;
+    UCSR0B = _BV(TXEN0) | _BV(RXEN0);
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+}
+
+void uart_write_char(char value)
+{
+    while ((UCSR0A & _BV(UDRE0)) == 0U)
+    {
+    }
+
+    UDR0 = (uint8_t)value;
+}
+
+void uart_write_string(const char *text)
+{
+    while (*text != '\0')
+    {
+        uart_write_char(*text);
+        text++;
+    }
+}
+
+void uart_write_uint16(uint16_t value)
+{
+    char buffer[6];
+    uint8_t index = 0U;
+
+    if (value == 0U)
+    {
+        uart_write_char('0');
+        return;
+    }
+
+    while (value > 0U && index < sizeof(buffer))
+    {
+        buffer[index++] = (char)('0' + (value % 10U));
+        value /= 10U;
+    }
+
+    while (index > 0U)
+    {
+        index--;
+        uart_write_char(buffer[index]);
+    }
+}
+
+void uart_write_newline(void)
+{
+    uart_write_char('\r');
+    uart_write_char('\n');
+}
---
+++ library/src/arduino_uart.h
@@ -0,0 +1,20 @@
+#ifndef ARDUINO_UART_H
+#define ARDUINO_UART_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void uart_init(uint32_t baud_rate);
+void uart_write_char(char value);
+void uart_write_string(const char *text);
+void uart_write_uint16(uint16_t value);
+void uart_write_newline(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
