e096f1d03ca35a91f078c9163019bf5a77a57995
akiratanaya <akirakenji78@gmail.com>
2026-05-13 23:06:28 +0700

    feat: gpio fundamental func

---
+++ library/src/arduino_gpio.c
@@ -0,0 +1,64 @@
+#include "arduino_gpio.h"
+
+#include "arduino_board.h"
+
+void pin_mode(uint8_t pin, board_pin_mode_t mode)
+{
+    volatile uint8_t *ddr = board_ddr(pin);
+    volatile uint8_t *port = board_port(pin);
+    uint8_t bit = board_bit(pin);
+
+    if (ddr == 0 || port == 0 || bit == 0U)
+    {
+        return;
+    }
+
+    if (mode == BOARD_OUTPUT)
+    {
+        *ddr |= bit;
+        *port &= (uint8_t)~bit;
+    }
+    else if (mode == BOARD_INPUT_PULLUP)
+    {
+        *ddr &= (uint8_t)~bit;
+        *port |= bit;
+    }
+    else
+    {
+        *ddr &= (uint8_t)~bit;
+        *port &= (uint8_t)~bit;
+    }
+}
+
+void digital_write(uint8_t pin, uint8_t value)
+{
+    volatile uint8_t *port = board_port(pin);
+    uint8_t bit = board_bit(pin);
+
+    if (port == 0 || bit == 0U)
+    {
+        return;
+    }
+
+    if (value != 0U)
+    {
+        *port |= bit;
+    }
+    else
+    {
+        *port &= (uint8_t)~bit;
+    }
+}
+
+uint8_t digital_read(uint8_t pin)
+{
+    volatile uint8_t *pin_reg = board_pin(pin);
+    uint8_t bit = board_bit(pin);
+
+    if (pin_reg == 0 || bit == 0U)
+    {
+        return 0U;
+    }
+
+    return ((*pin_reg & bit) != 0U) ? 1U : 0U;
+}
