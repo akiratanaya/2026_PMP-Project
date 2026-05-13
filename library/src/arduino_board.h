9f44db86bec6746904a72209335ab5549f866f14
akiratanaya <akirakenji78@gmail.com>
2026-05-13 23:04:23 +0700

    feat: add port assign

---
+++ library/src/arduino_board.h
@@ -0,0 +1,23 @@
+#ifndef ARDUINO_BOARD_H
+#define ARDUINO_BOARD_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void board_init(void);
+
+uint8_t board_pin_count(void);
+volatile uint8_t *board_ddr(uint8_t pin);
+volatile uint8_t *board_port(uint8_t pin);
+volatile uint8_t *board_pin(uint8_t pin);
+uint8_t board_bit(uint8_t pin);
+uint8_t board_pwm_pin_valid(uint8_t pin);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
