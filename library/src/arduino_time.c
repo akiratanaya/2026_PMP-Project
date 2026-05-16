1b0f855cb633c79695a2aaa940182c477698560b
akiratanaya <akirakenji78@gmail.com>
2026-05-17 03:27:22 +0700

    feat: add delay function

---
+++ library/src/arduino_time.c
@@ -0,0 +1,12 @@
+#include "arduino_time.h"
+
+#include <util/delay.h>
+
+void delay_ms(uint16_t ms)
+{
+    while (ms > 0U)
+    {
+        _delay_ms(1.0);
+        ms--;
+    }
+}
---
+++ library/src/arduino_time.h
@@ -0,0 +1,16 @@
+#ifndef ARDUINO_TIME_H
+#define ARDUINO_TIME_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void delay_ms(uint16_t ms);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
